d57c53bcfce1cdabc01afff7265866f9c3b43244
nezkh <f.marchant.aravena@gmail.com>
2022-09-22 20:21:52 -0300

    carga del objeto desde un fichero

--- ejercicio8_2.py
+++ ejercicio8_2.py
@@ -1,12 +1,23 @@
 import pickle
 
+# create vehicle class
 class Vehiculo:
+        def __init__(self, modelo, velocidad, puertas, color):
+            self.modelo = modelo
+            self.velocidad = velocidad
+            self. puertas = puertas
+            self.color = color
+        def __str__(self):
+            return f'el auto es un {self.modelo} tiene una velocidad maxima de {self.velocidad} es de {self.puertas} puertas y es de color {self.color}'
     
-    def __init__(self, velocidad, puertas, ruedas, color):
-        self.velocidad = velocidad
-        self. puertas = puertas
-        self.ruedas = ruedas
-        self.color = color
+# create object and print
+auto = Vehiculo("Golf",188, 5, "Gris")
+print(auto)
 
-    def __str__(self):
-        return 
+f = open('vehiculo_obj.bin', 'wb')
+pickle.dump(auto, f)
+f.close()
+
+f = open('vehiculo_obj.bin', 'rb')
+auto = pickle.load(f)
+f.close()
