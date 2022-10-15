e404519688a541d77dd599a5031ecc8d5c4c7d82
nezkh <f.marchant.aravena@gmail.com>
2022-10-15 14:25:19 -0300

    fix security

--- Tema 12/ejercicio12/ejercicio12/settings.py
+++ Tema 12/ejercicio12/ejercicio12/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from xmlrpc.client import boolean
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xmz-u(atkzovxdv%uc)4o2g7=-2uvj*z+d5v@3^g*xcj10tm0^'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
